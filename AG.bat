bf34db62f1df43d42b74ea6dbea38363b8a6d62e
José Nelson <josefcultrifrc@gmail.com>
2022-11-23 10:02:28 -0300

    Acrescentando cópia de dll da base AG

--- AG.bat
+++ AG.bat
@@ -36,18 +36,23 @@ xcopy %PathAG%*rlb*.js %PathApp% /D:%1 /Y
 cd bin
 xcopy %PathAG%bin\*tfr* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wfr* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wsfr* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*rfr* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*tsc* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wsc* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wssc* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*rsc* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*tcn* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wcn* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wscn* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*rcn* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*tca* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wca* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wsca* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*rca* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*tag* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wag* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wsag* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*rag* %PathApp%\bin /D:%1 /Y
 
 xcopy %PathAG%bin\*psc* %PathApp%\bin /D:%1 /Y
@@ -55,6 +60,7 @@ xcopy %PathAG%bin\*apag* %PathApp%\bin /D:%1 /Y
 
 xcopy %PathAG%bin\*tfa* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wfa* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wsfa* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*rfa* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*pes* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*pfr* %PathApp%\bin /D:%1 /Y
@@ -65,14 +71,17 @@ xcopy %PathAG%bin\*pfr* %PathApp%\bin /D:%1 /Y
 
 xcopy %PathAG%bin\*tmi* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wmi* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wsmi* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*rmi* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*pmi* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*tlb* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wlb* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wslb* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*rlb* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*plb* %PathApp%\bin /D:%1 /Y
 
 xcopy %PathAG%bin\*tef* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*wef* %PathApp%\bin /D:%1 /Y
+xcopy %PathAG%bin\*wsef* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*ref* %PathApp%\bin /D:%1 /Y
 xcopy %PathAG%bin\*pef* %PathApp%\bin /D:%1 /Y

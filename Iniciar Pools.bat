7ae286de10965e96c5f4bb6d6c90ef6c9f5d46e5
José Nelson <josefcultrifrc@gmail.com>
2022-11-29 16:12:37 -0300

    Ajustando pools a serem iniciados

--- Iniciar Pools.bat
+++ Iniciar Pools.bat
@@ -1,13 +1,11 @@
 title "Iniciando Pools"
 
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWEBORA_GX15
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWEBPOS_GX15
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWEBSQL_GX15
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:Atualizador
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHB_MINERADOR
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBAGRO
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBAGRO2
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWEB
+%windir%\system32\inetsrv\appcmd start apppool /apppool.name:"ASP.NET V4.0"
+%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBAGRO_GX15
+%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWEBORA_GX17
+%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWEBPOS_GX17
+%windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWEBSQL_GX17
 %windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWS
 %windir%\system32\inetsrv\appcmd start apppool /apppool.name:CHBWS15
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:ORACLE
+%windir%\system32\inetsrv\appcmd start apppool /apppool.name:DefaultAppPool
+%windir%\system32\inetsrv\appcmd start apppool /apppool.name:SUPORTEWEBWS
